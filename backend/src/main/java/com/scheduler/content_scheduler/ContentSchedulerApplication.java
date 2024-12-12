e8075565852002b58eb38483db34153e167d7f95
Giuseppe Desiato <giodesiato@gmail.com>
2024-12-12 19:20:55 +0100

    backend project

---
+++ backend/src/main/java/com/scheduler/content_scheduler/ContentSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.scheduler.content_scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContentSchedulerApplication.class, args);
+	}
+
+}
