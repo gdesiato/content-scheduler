dda49f91f6da56dd92effdf525f78efc6c6e670d
Giuseppe Desiato <giodesiato@gmail.com>
2024-12-12 19:20:55 +0100

    backend project

---
+++ backend/src/test/java/com/scheduler/content_scheduler/ContentSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scheduler.content_scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
