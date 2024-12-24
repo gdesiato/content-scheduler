6fcaca58b18bcc26de0883f8e776ede3a0c78591
Giuseppe Desiato <giodesiato@gmail.com>
2024-12-24 18:12:10 +0100

    create and edit user

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,12 +1,15 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { UserListComponent } from './user-management/user-list/user-list.component';
-import { PostListComponent } from './post-management/post-list/post-list.component';
+import { CreateUserComponent } from './user-management/create-user/create-user.component';
+import { EditUserComponent } from './user-management/edit-user/edit-user.component';
 import { AuthGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent }, // Public route
-  { path: 'users', component: UserListComponent, canActivate: [AuthGuard] }, // Protected route
-  { path: 'posts', component: PostListComponent, canActivate: [AuthGuard] }, // Protected route
-  { path: '', redirectTo: 'login', pathMatch: 'full' }, // Redirect to login by default
+  { path: 'users', component: UserListComponent, canActivate: [AuthGuard] }, // User list
+  { path: 'user/new', component: CreateUserComponent, canActivate: [AuthGuard] }, // Create user
+  { path: 'user/:id', component: EditUserComponent, canActivate: [AuthGuard] }, // Edit user
+  { path: '', redirectTo: 'login', pathMatch: 'full' }, // Default redirect
+  { path: '**', redirectTo: 'login', pathMatch: 'full' }, // Wildcard route
 ];
