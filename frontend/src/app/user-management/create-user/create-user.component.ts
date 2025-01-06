abd4ec762acecef067b54f7522241dcd241422a3
Giuseppe Desiato <giodesiato@gmail.com>
2025-01-06 01:01:08 +0100

    admin create user page

--- frontend/src/app/user-management/create-user/create-user.component.ts
+++ frontend/src/app/user-management/create-user/create-user.component.ts
@@ -2,11 +2,15 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { UserService } from '../user.service';
 import { UserRequestDTO } from '../../models/user-request.dto';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-create-user',
+  standalone: true,
+  imports: [FormsModule, CommonModule],
   templateUrl: './create-user.component.html',
-  styleUrls: ['./create-user.component.css']
+  styleUrls: ['./create-user.component.css'],
 })
 export class CreateUserComponent {
   user: UserRequestDTO = { username: '', email: '', password: '' };
@@ -17,7 +21,7 @@ export class CreateUserComponent {
   createUser(): void {
     this.userService.createUser(this.user).subscribe({
       next: () => this.router.navigate(['/users']),
-      error: (err) => this.errorMessage = 'Failed to create user'
+      error: (err) => (this.errorMessage = 'Failed to create user'),
     });
   }
 }
