8aca4dabe170568f3640e930867d7587bdc26338
Giuseppe Desiato <giodesiato@gmail.com>
2025-01-09 21:05:29 +0100

    frontend correctly creates users

--- frontend/src/app/guards/auth.guard.ts
+++ frontend/src/app/guards/auth.guard.ts
@@ -17,7 +17,7 @@ export class AuthGuard implements CanActivate {
     if (this.authService.isAuthenticated()) {
       return true;
     } else {
-      this.router.navigate(['/login']);  // Redirect to login if not authenticated
+      this.router.navigate(['/login']);
       return false;
     }
   }
