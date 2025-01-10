56b411474ed28ca7456c8561b6dab52cbb55436a
Giuseppe Desiato <giodesiato@gmail.com>
2025-01-10 08:29:16 +0100

    added edit button to create user

--- backend/src/main/java/com/scheduler/content_scheduler/service/UserService.java
+++ backend/src/main/java/com/scheduler/content_scheduler/service/UserService.java
@@ -6,6 +6,8 @@ import com.scheduler.content_scheduler.exception.UserNotFoundException;
 import com.scheduler.content_scheduler.mapper.UserMapper;
 import com.scheduler.content_scheduler.model.UserEntity;
 import com.scheduler.content_scheduler.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +15,8 @@ import java.util.List;
 
 @Service
 public class UserService {
+
+    private static final Logger log = LoggerFactory.getLogger(UserService.class);
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
@@ -31,18 +35,26 @@ public class UserService {
 
     public UserResponseDTO getUserById(Long id) {
         UserEntity user = userRepository.findById(id)
-                .orElseThrow(() -> new UserNotFoundException("User with ID {} not found." + id));
+                .orElseThrow(() -> new UserNotFoundException(String.format("User with ID %d not found.", id)));
         return UserMapper.toDTO(user);
     }
 
     public UserResponseDTO getUserByUsername(String username) {
         UserEntity user = userRepository.findByUsername(username)
-                .orElseThrow(() -> new UserNotFoundException("User with username {} not found." + username));
+                .orElseThrow(() -> new UserNotFoundException(String.format("User with username %s not found.", username)));
         return UserMapper.toDTO(user);
     }
 
     public UserResponseDTO createUser(UserRequestDTO userRequestDTO) {
         UserEntity user = UserMapper.toEntity(userRequestDTO);
+
+        System.out.println("Password before hashing: " + userRequestDTO.password());
+
+        if (user.getPassword() != null && !user.getPassword().isEmpty()) {
+            user.setPassword(passwordEncoder.encode(user.getPassword()));
+        }
+        System.out.println("Password after hashing: " + passwordEncoder.encode(userRequestDTO.password()));
+
         UserEntity savedUser = userRepository.save(user);
         return UserMapper.toDTO(savedUser);
     }
@@ -53,7 +65,11 @@ public class UserService {
                 .orElseThrow(() -> new UserNotFoundException("User not found"));
 
         existingUser.setUsername(updatedUserData.username());
-        existingUser.setPassword(passwordEncoder.encode(updatedUserData.password()));
+
+        if (updatedUserData.password() != null && !updatedUserData.password().isEmpty()) {
+            existingUser.setPassword(passwordEncoder.encode(updatedUserData.password()));
+        }
+
         existingUser.setRole(updatedUserData.role());
 
         UserEntity updatedUser = userRepository.save(existingUser);
